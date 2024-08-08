4ed10ec665bf200ad11359886d20cfc2e382ad66
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-08-08 19:27:00 -0400

    the

---
+++ ASS Compiler/Program.cs
@@ -0,0 +1,41 @@
+﻿string location = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+string[] files = Directory.GetFiles(location);
+
+string[] assFiles = {};
+
+foreach(var file in files)
+{
+    string fileType = file.Substring(file.Length-4,4);
+
+    if(fileType == ".ASS")
+    {
+        string endFile = Path.GetFileName(file);
+
+        assFiles.Append(endFile);
+        
+        Console.WriteLine(endFile);
+    }
+}
+
+string selectedFile = null;
+
+while(selectedFile==null)
+{
+    Console.WriteLine("Select an ASS program from the above to run");
+
+    string input = Console.ReadLine();
+
+    foreach(var file in assFiles)
+    {
+        Console.WriteLine(input);
+        Console.WriteLine(file);
+        if(string.Compare(file,input)==1)
+        {
+            selectedFile=file;
+            break;
+        }
+    }
+}
+
+Console.WriteLine("running " + selectedFile + "...");
